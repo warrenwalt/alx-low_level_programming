ecdfcdac7c68b6e177d92340aab0e29cc1b5d368
warrenwalt <warrenwalt19@gmail.com>
2022-04-21 02:36:29 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints character in a string in a new line
+ * @str: value to be evaluated
+ * Return : not
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0; z<= 1; z++)
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
+}
