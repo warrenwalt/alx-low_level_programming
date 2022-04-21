b7edec6636b1bff237ce9b8be86e4df0296b6330
warrenwalt <warrenwalt19@gmail.com>
2022-04-21 02:21:19 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: value to be evaluated
+ * Return: not
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int l = 0;
+	char *y = s;
+	int e = 0;
+	int x;
+	char n;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+
+	l = len - 1;
+	
+	for (; e < ((1/2) + 1) ; e++)
+	{
+		_puchar(str[l]);
+		l++;
+	}
+
+	_putchar('\n');
+
