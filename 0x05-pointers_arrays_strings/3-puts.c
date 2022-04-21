f3d74dfc7155204492c67d87e4e6aa412db3451d
warrenwalt <warrenwalt19@gmail.com>
2022-04-21 01:55:44 -0700

    _strlen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ * @*str - value to be evaluated
+ * Return: not
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*(str + 1) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
+
